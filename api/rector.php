e90411fb1d62fa6a7c5961ae8c779a2a9dbc99ef
Alexandr Bubeshko <a.bubeshko@gmail.com>
2023-09-03 22:22:19 +0300

    chore: add firebase/php-jwt package to the project

--- api/rector.php
+++ api/rector.php
@@ -18,7 +18,7 @@ return static function (RectorConfig $config): void {
     $config->cacheDirectory(__DIR__ . '/var/cache/rector');
 
     $config->sets([
-        LevelSetList::UP_TO_PHP_81,
+        LevelSetList::UP_TO_PHP_82,
         SetList::DEAD_CODE,
     ]);
 
