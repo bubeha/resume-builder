fdc06f284caf0a945d0c5660d37afa56f25f7db1
Alexandr Bubeshko <a.bubeshko@gmail.com>
2023-09-03 14:08:26 +0300

    test: add UuidGeneratorTest.php

---
+++ api/tests/App/Infrastructure/Persistence/Doctrine/Generator/UuidGeneratorTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\App\Infrastructure\Persistence\Doctrine\Generator;
+
+use App\Domain\ValueObjects\Uuid;
+use App\Infrastructure\Persistence\Doctrine\Generator\UuidGenerator;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Mapping\Entity;
+use PHPUnit\Framework\TestCase;
+
+final class UuidGeneratorTest extends TestCase
+{
+    public function testUuidGeneratorGenerate(): void
+    {
+        /** @var \Doctrine\ORM\EntityManagerInterface $em */
+        $em = self::createMock(EntityManager::class);
+
+        $entity = new Entity();
+        $generator = new UuidGenerator();
+
+        $uuid = $generator->generate($em, $entity);
+
+        self::assertInstanceOf(Uuid::class, $uuid);
+    }
+}
