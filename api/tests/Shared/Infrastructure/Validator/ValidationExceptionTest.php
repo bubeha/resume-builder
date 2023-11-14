0802373b094470f6229ee967831f20542fbd455a
Alexandr Bubeshko <a.bubeshko@gmail.com>
2023-11-14 20:50:49 +0300

    test: add validation tests

---
+++ api/tests/Shared/Infrastructure/Validator/ValidationExceptionTest.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Shared\Infrastructure\Validator;
+
+use App\Shared\Infrastructure\Validator\ValidationException;
+use PHPUnit\Framework\MockObject\Exception;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Validator\ConstraintViolationListInterface;
+
+final class ValidationExceptionTest extends TestCase
+{
+    /**
+     * @throws Exception
+     */
+    public function testException(): void
+    {
+        $constraintMock = $this->createMock(ConstraintViolationListInterface::class);
+
+        self::assertSame(
+            $constraintMock,
+            (new ValidationException($constraintMock))->getViolations(),
+        );
+    }
+}
