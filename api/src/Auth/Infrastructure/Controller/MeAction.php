23b430e02b5fb925ad17544384de4b9de78fb23a
Alexandr Bubeshko <a.bubeshko@gmail.com>
2023-12-27 22:27:13 +0300

    feat: add base me response

--- api/src/Auth/Infrastructure/Controller/MeAction.php
+++ api/src/Auth/Infrastructure/Controller/MeAction.php
@@ -5,17 +5,24 @@ declare(strict_types=1);
 namespace App\Auth\Infrastructure\Controller;
 
 use App\Shared\Domain\Entities\AuthenticatedUser;
+use App\Shared\Domain\Entities\User;
 use App\Shared\Infrastructure\Controller\AbstractAction;
 use Psr\Http\Message\ResponseInterface as Response;
+use Webmozart\Assert\Assert;
 
 final class MeAction extends AbstractAction
 {
     public function __construct(
-        private readonly ?AuthenticatedUser $user,
+        private readonly AuthenticatedUser $user,
     ) {}
 
     public function handle(): Response
     {
-        return $this->json($this->user);
+        Assert::isInstanceOf($this->user, User::class);
+
+        return $this->json([
+            'id' => (string)$this->user->getIdentifier(),
+            'email' => (string)$this->user->getEmail(),
+        ]);
     }
 }
